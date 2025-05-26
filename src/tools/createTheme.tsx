328a413cbd31396cd1c840b3b8865ac0438064d1
Oliver Winter <owinter86@gmail.com>
2025-05-26 13:59:13 +0200

    fix: update create theme

--- src/tools/createTheme.tsx
+++ src/tools/createTheme.tsx
@@ -13,17 +13,6 @@ import type {
 	TextVariant,
 } from "../types";
 
-export const breakpoints = {
-	xs: 0,
-	sm: 576,
-	md: 768,
-	lg: 992,
-	xl: 1200,
-	superLarge: 2000,
-	tvLike: 4000,
-} as const;
-type AppBreakpoints = typeof breakpoints;
-
 type ThemeType<
 	T extends LightColors,
 	K extends FontMetrics,
@@ -39,13 +28,12 @@ type ThemeType<
 
 // if you defined themes
 type AppThemes = {
-	light: ReturnType<typeof createtheme>["themes"]["light"];
-	dark: ReturnType<typeof createtheme>["themes"]["dark"];
+	light: ReturnType<typeof createtheme>["light"];
+	dark: ReturnType<typeof createtheme>["dark"];
 };
 
 declare module "react-native-unistyles" {
 	export interface UnistylesThemes extends AppThemes {}
-	export interface UnistylesBreakpoints extends AppBreakpoints {}
 }
 
 export function createtheme<
@@ -59,12 +47,8 @@ export function createtheme<
 >(
 	config: Omit<CreateLBConfig<K, T, S, Spacing, Radius, TTextVariant, TButtonVariant>, "capsize">
 ): {
-	breakpoints: AppBreakpoints;
-	settings: { adaptiveThemes: true };
-	themes: {
-		light: ThemeType<T, K, S, Spacing, Radius, TTextVariant, TButtonVariant>;
-		dark: ThemeType<T, K, S, Spacing, Radius, TTextVariant, TButtonVariant>;
-	};
+	light: ThemeType<T, K, S, Spacing, Radius, TTextVariant, TButtonVariant>;
+	dark: ThemeType<T, K, S, Spacing, Radius, TTextVariant, TButtonVariant>;
 } {
 	const themeColors = {
 		light: config.colors.light,
@@ -94,9 +78,5 @@ export function createtheme<
 	const lightTheme = { ...config, capsize: fonts, colors: themeColors.light };
 	const darkTheme = { ...config, capsize: fonts, colors: themeColors.dark };
 
-	return {
-		breakpoints,
-		settings: { adaptiveThemes: true },
-		themes: { light: lightTheme, dark: darkTheme },
-	};
+	return { light: lightTheme, dark: darkTheme };
 }
